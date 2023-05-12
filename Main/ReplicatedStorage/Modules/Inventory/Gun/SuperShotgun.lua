f4b9ba6a10dab7cb0aa2ac80e5e04ccdea90776d
BurntWaffleCake <94855083+BurntWaffleCake@users.noreply.github.com>
2023-05-11 20:59:07 -0500

    Create SuperShotgun.lua

---
+++ Main/ReplicatedStorage/Modules/Inventory/Gun/SuperShotgun.lua
@@ -0,0 +1,46 @@
+local RepStor = game:GetService("ReplicatedStorage")
+--local AnimationRepo = RepStor.Assets.Animations.Tools.BasicShotgun
+
+local Gun = require(script.Parent)
+
+local BasicShotgun = {}
+BasicShotgun.__index = BasicShotgun
+setmetatable(BasicShotgun, Gun)
+
+function BasicShotgun.new(owner: Player, ignore: {Instance})
+	local newBasicShotgun: Gun.Gun = Gun.new(script.Name, owner, ignore, "Rifle")
+
+	newBasicShotgun.Firerate = 150
+	newBasicShotgun.Firemode = "Semi" -- "Semi", "Auto"
+
+	newBasicShotgun.Ammo = 1
+	newBasicShotgun.MagSize = 1
+	newBasicShotgun.ShotCount = 32
+	newBasicShotgun.Reserve = math.huge
+
+	newBasicShotgun.ReloadTime = 1
+
+	newBasicShotgun.VerticalSpread = Vector2.new(-5.5,5.5)
+	newBasicShotgun.HorizontalSpread = Vector2.new(-5.5,5.5)
+	
+	newBasicShotgun.VerticalRecoil = Vector2.new(300,350)
+	newBasicShotgun.HorizontalRecoil = Vector2.new(-100,100)
+	
+	newBasicShotgun.Projectile = RepStor.Modules.ProjectileModules.Projectiles.ShotgunPellet  
+
+	newBasicShotgun.OnShot.Event:Connect(function()
+		newBasicShotgun.ViewModel.Model.Main.FirePart.MuzzleFlash:Emit(1)
+	--	newBasicShotgun.ViewModel:PlayAnimation(AnimationRepo.BasicShotgun_Shoot, nil, nil, 2)
+	--	newBasicShotgun.ViewModel:PlaySound("Shot")
+	--	task.wait(.3)
+	--	newBasicShotgun.ViewModel:PlaySound("Pump")
+	end)
+	
+	newBasicShotgun.OnReload.Event:Connect(function()
+	--	newBasicShotgun.ViewModel:PlaySound("Reload")
+	end)
+
+	return newBasicShotgun
+end
+
+return BasicShotgun
