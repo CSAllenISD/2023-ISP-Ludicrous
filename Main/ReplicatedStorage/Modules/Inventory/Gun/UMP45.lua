1758091d88891950af8ba4c0239e844e43d92f3f
BurntWaffleCake <94855083+BurntWaffleCake@users.noreply.github.com>
2023-05-11 20:59:39 -0500

    Create UMP45.lua

---
+++ Main/ReplicatedStorage/Modules/Inventory/Gun/UMP45.lua
@@ -0,0 +1,40 @@
+local RepStor = game:GetService("ReplicatedStorage")
+local AnimationRepo = RepStor.Assets.Animations.Tools.AK47
+
+local Gun = require(script.Parent)
+
+local AK47 = {}
+AK47.__index = AK47
+setmetatable(AK47, Gun)
+
+function AK47.new(owner: Player, ignore: {Instance})
+	local newAK47: Gun.Gun = Gun.new(script.Name, owner, ignore, "Rifle")
+
+	newAK47.Firerate = 750
+	newAK47.Firemode = "Auto" -- "Semi", "Auto"
+
+	newAK47.Ammo = 25
+	newAK47.MagSize = 25
+	newAK47.Reserve = math.huge
+
+	newAK47.ReloadTime = 1.3
+
+	newAK47.VerticalRecoil = Vector2.new(-20,20)
+	newAK47.HorizontalRecoil = Vector2.new(-20,20)
+	
+	newAK47.Projectile = RepStor.Modules.ProjectileModules.Projectiles.Bullet
+
+	newAK47.OnShot.Event:Connect(function()
+		newAK47.ViewModel.Model.Main.FirePart.MuzzleFlash:Emit(1)
+		newAK47.ViewModel:PlayAnimation(AnimationRepo.AK47_Shoot, nil, nil, 5)
+		newAK47.ViewModel:PlaySound("Fire")
+	end)
+	
+	newAK47.OnReload.Event:Connect(function()
+		newAK47.ViewModel:PlaySound("Reload")
+	end)
+
+	return newAK47
+end
+
+return AK47
